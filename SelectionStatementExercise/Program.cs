984b9a7acc41ebec96b7488ff9892846ce9d8404
Daniel Finch <xiphoid0311@gmail.com>
2023-05-29 23:51:53 -0500

    Made random number guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello you! Try and guess my favorite number!");
+
+          var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+           var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber) 
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else 
+            { 
+                Console.WriteLine("Congradulations!");
+            }
+
         }
     }
 }
